e695f2a825e808344423b23e89dc786e0111c349
KOOSANGYOON <nevergiveup936@naver.com>
2017-11-16 10:34:18 +0900

    POKEMON GAME
    1. delete useless images
    2. add some pokemons

--- jsPokemon/pokemon.js
+++ jsPokemon/pokemon.js
@@ -56,31 +56,39 @@ function generateArray(enemy, enemyPkm, myPkm) {
   enemy[2] = "POBI";
   enemy[3] = "HONUX";
 
-  var mon1 = new makePokemon("피카츄", 300, 300, 100, "pikachu.gif");
-  var mon2 = new makePokemon("파이리", 300, 300, 60, "Charmander.gif");
-  var mon3 = new makePokemon("꼬부기", 200, 200, 60, "Squirtle.gif");
-  var mon4 = new makePokemon("이상해씨", 200, 200, 60, "Bulbasaur.gif");
-  var mon5 = new makePokemon("이브이", 150, 150, 80, "Eevee.gif");
-  var mon6 = new makePokemon("망키", 500, 500, 10, "Mankey.gif");
-  var mon7 = new makePokemon("고라파덕", 50, 50, 200, "Psyduck.gif");
+  var mon0 = new makePokemon("캐터피", 50, 50, 10, "Caterpie.gif");
+  var mon1 = new makePokemon("이브이", 150, 150, 60, "Eevee.gif");
+  var mon2 = new makePokemon("꼬부기", 200, 200, 60, "Squirtle.gif");
+  var mon3 = new makePokemon("이상해씨", 220, 220, 60, "Bulbasaur.gif");
+  var mon4 = new makePokemon("파이리", 250, 250, 60, "Charmander.gif");
+  var mon5 = new makePokemon("고라파덕", 50, 50, 300, "Psyduck.gif");
+  var mon6 = new makePokemon("피카츄", 300, 300, 100, "pikachu.gif");
+  var mon7 = new makePokemon("우츠보트", 500, 500, 150, "uchboat.gif");
+  var mon8 = new makePokemon("잠만보", 1000, 1000, 100, "zambo.png");
+  var mon9 = new makePokemon("[보스]", 1500, 1500, 400, "gara.png");
 
-  var myMon1 = new makePokemon("피카츄", 300, 300, 120, "pikachuBack.png");
-  var myMon2 = new makePokemon("파이리", 100, 100, 100, "charmanderBack.png");
-  var myMon3 = new makePokemon("꼬부기", 300, 300, 100, "squirtleBack.png");
-  var myMon4 = new makePokemon("이상해씨", 300, 300, 100, "bulbasaurBack.png");
+  var myMon0 = new makePokemon("피카츄", 300, 300, 120, "pikachuBack.png");
+  var myMon1 = new makePokemon("파이리", 100, 100, 100, "charmanderBack.png");
+  var myMon2 = new makePokemon("꼬부기", 300, 300, 100, "squirtleBack.png");
+  var myMon3 = new makePokemon("이상해씨", 300, 300, 100, "bulbasaurBack.png");
+  var myMon4 = new makePokemon("짱짱보스", 3000, 3000, 2000, "boss.png");
 
-  enemyPkm[0] = mon1;
-  enemyPkm[1] = mon2;
-  enemyPkm[2] = mon3;
-  enemyPkm[3] = mon4;
-  enemyPkm[4] = mon5;
-  enemyPkm[5] = mon6;
-  enemyPkm[6] = mon7;
+  enemyPkm[0] = mon0;
+  enemyPkm[1] = mon1;
+  enemyPkm[2] = mon2;
+  enemyPkm[3] = mon3;
+  enemyPkm[4] = mon4;
+  enemyPkm[5] = mon5;
+  enemyPkm[6] = mon6;
+  enemyPkm[7] = mon7;
+  enemyPkm[8] = mon8;
+  enemyPkm[9] = mon9;
 
-  myPkm[0] = myMon1;
-  myPkm[1] = myMon2;
-  myPkm[2] = myMon3;
-  myPkm[3] = myMon4;
+  myPkm[0] = myMon0;
+  myPkm[1] = myMon1;
+  myPkm[2] = myMon2;
+  myPkm[3] = myMon3;
+  myPkm[4] = myMon4;
 }
 
 //draw enemy coming zone
@@ -120,7 +128,7 @@ function drawEnemy(arr, pos, monNum) {
   enemyImage.onload = function () {
     drawE.drawImage(enemyImage, pos[1], -10);
   };
-  for (var i = monNum; i < 7; i++) {
+  for (var i = monNum; i < 10; i++) {
     if (arr[i].hp > 0) {
       enemyImage.src = arr[i].src;
       drawEnemyStatus(arr, i);
@@ -143,7 +151,7 @@ function drawMe(arr, pos, monNum) {
   pkm2Image.onload = function () {
     drawM.drawImage(pkm2Image, pos[0], 10);
   };
-  for (var i = monNum; i < 4; i++) {
+  for (var i = monNum; i < 5; i++) {
     if (arr[i].hp > 0) {
       pkm2Image.src = arr[i].src;
       drawMyStatus(arr, i);
